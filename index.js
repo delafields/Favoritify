01314a2fb584bdb3cee919cb2b5760f914d6e2d1
badatfun <j.c.delafields@gmail.com>
2017-10-16 17:58:35 -0400

    Initial passport setup

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const express = require('express');
+const passport = require('passport');
+const SpotifyStrategy = require('passport-spotify').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send({ Im: 'Working Fam' });
-});
+passport.use(
+	new SpotifyStrategy(
+		{
+			clientID: keys.SPOTIFY_CLIENT_ID,
+			clientSecret: keys.SPOTIFY_CLIENT_SECRET,
+			callbackURL: '/auth/spotify/callback'
+		},
+		(accessToken, refreshToken, profile, done) => {
+			console.log('accessToken: ', accessToken);
+			console.log('refreshToken: ', refreshToken);
+			console.log('profile: ', profile);
+		}
+	)
+);
+
+app.get(
+	'/auth/spotify',
+	passport.authenticate('spotify', {
+		scope: ['user-read-email', 'user-top-read']
+	})
+);
+
+app.get('/auth/spotify/callback', passport.authenticate('spotify'));
 
 const PORT = process.env.PORT || 5000;
 
