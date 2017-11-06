f8426d27ec0d0fd50e8c7992fa8650cc813f55a5
badatfun <j.c.delafields@gmail.com>
2017-11-06 15:41:40 -0500

    Add postbuild script. Added express production handling

--- index.js
+++ index.js
@@ -27,6 +27,17 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
+// Serve production assets
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+
+	// Serve index.html if route is unrecognized
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
